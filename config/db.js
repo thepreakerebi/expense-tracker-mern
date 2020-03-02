1eadd93c77d7c8e65cf1089ba38935a0a601996e
coolPreye <kerebipreye@gmail.com>
2020-03-02 01:00:42 -0800

    first commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const colors = require('colors');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true
+        });
+
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+    } catch (err) {
+        console.log(`Error: ${err.message}`.purple);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
---
+++ routes/transactions.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { getTransactions, addTransaction, deleteTransaction } = require('../controllers/transactions');
+
+router
+    .route('/')
+    .get(getTransactions)
+    .post(addTransaction);
+
+router
+    .route('/:id')
+    .delete(deleteTransaction);
+
+module.exports = router;
